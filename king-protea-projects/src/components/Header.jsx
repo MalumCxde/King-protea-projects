b19dbb033aa8d89b5705a8e2051b8af423727ba0
SizweMic <192145476@my.cjc.edu.za>
2024-06-08 11:09:51 +0200

    modified:   king-protea-projects/src/components/Header.jsx

--- king-protea-projects/src/components/Header.jsx
+++ king-protea-projects/src/components/Header.jsx
@@ -3,7 +3,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
 import '../styles/styles.css';
 
 const Header = () => {
@@ -43,6 +42,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
